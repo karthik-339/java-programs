305380b8ea7f5ad3ddf3ee26df6a319a7daea4d7
BADDAM KARTHIK REDDY <2410030339@klh.edu.in>
2025-10-31 00:00:51 +0530

    Add files via upload

---
+++ MaxNumber.java
@@ -0,0 +1,22 @@
+package S4.Arrays;
+
+import java.util.Scanner;
+
+public class MaxNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum number in the array is: " + max);
+        scanner.close();
+    }
+}
